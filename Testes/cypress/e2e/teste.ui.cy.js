7c5f20c6f7aa65d2e8ecb799f74e95bd0168dafc
JoaoIgreja <jvigreja@gmail.com>
2023-11-13 22:41:06 -0300

    commit

--- Testes/cypress/e2e/teste.ui.cy.js
+++ Testes/cypress/e2e/teste.ui.cy.js
@@ -1,10 +1,13 @@
 /// <reference types ="cypress"/>
 
-describe('Criando cenário de teste para o site Kabum', () => {
+describe('Criando cenário de teste para o site Uai Rango', () => {
 
-  it('Caso de teste: Pesquisa de Items', () => {
+  it('Caso de teste: Pesquisa de Items e acesso', () => {
 
-      cy.visit('https://www.kabum.com.br')
+      cy.visit('https://www.uairango.com/delivery/santa-rita-do-sapucai')
+      cy.get('#palavra').type('Restaurante Elite Mineira')
+      cy.get('#palavra').type('{enter}');
+      cy.get('.foto-estabelecimento').click()
 
   })
 
@@ -22,44 +25,22 @@ function criarUsuario(){
   let minutos = new Date().getMinutes().toString()
   let segundos = new Date().getSeconds().toString()
   let nome = 'Joao Testador de site'
-  let cpf = '963.230.617-16'
-  let nascimento = '23' + '10' + '2002'
   let telefone = '27' + '998734212'
-  let email = horas + minutos + '@gmail.com'
+  let email = horas + minutos + 'Teste' + '@gmail.com'
   let senha = horas + minutos + segundos + 'Senha'
-  let userInfo = [nome, cpf, nascimento, telefone, email, senha]
-  let cep = '29103472'
-  let identi = 'Casa'
-  let rua = 'Rua Flávio da Vitória'
-  let numero = '9'
-  let complemento = '5'
-  let bairro = 'Jardim Guaranhuns'
-  let cidade = 'Vila Velha'
-  let uf = 'ES'
+  let userInfo = [nome, telefone, email, senha]
 
-  cy.visit('https://www.kabum.com.br/login?redirect_uri=https://www.kabum.com.br/')
-  cy.get('.sc-kNecGe > a').click()
-  cy.get(':nth-child(1) > :nth-child(1) > .sc-gweoQa > .sc-eIcdZJ > input').type(nome)
-  cy.get(':nth-child(1) > :nth-child(2) > .sc-gweoQa > .sc-eIcdZJ > input').type(cpf)
-  cy.get(':nth-child(2) > :nth-child(1) > .sc-gweoQa > .sc-eIcdZJ > input').type(nascimento)
-  cy.get(':nth-child(2) > :nth-child(2) > .sc-gweoQa > .sc-eIcdZJ > input').type(telefone)
-  cy.get(':nth-child(3) > .sc-la-DkbX > .sc-gweoQa > .sc-eIcdZJ > input').type(email)
-  cy.get(':nth-child(4) > :nth-child(1) > .sc-gweoQa > .sc-eIcdZJ > input').type(senha)
-  cy.get(':nth-child(4) > :nth-child(2) > .sc-gweoQa > .sc-eIcdZJ > input').type(senha)
-  cy.get('.sc-gvPdwL').click()
-  cy.get('.sc-djVXDX > :nth-child(2)').click()
-  cy.get(':nth-child(8) > .sc-fyVfxW > .sc-eHsDsR > .inputMark').click
-  cy.get('.sc-ikkxIA').click()
+  cy.visit('https://www.uairango.com/delivery/santa-rita-do-sapucai')
+  cy.get('.modal_login').click()
+  cy.get('.infos-login > .criar').click()
+  cy.get('#nome').type(nome)
+  cy.get('#tel1').type(telefone)
+  cy.get('#email').type(email)
+  cy.get('#senha').type(senha)
+  cy.get('#senha2').type(senha)
+  cy.get('.checa-politica').click()
+  cy.get('.infos-cadastro > .form-login > #form_site > [name="envia"]').click()
 
-  cy.get(':nth-child(2) > :nth-child(1) > .sc-gweoQa > .sc-eIcdZJ > input').type(cep)
-  cy.get(':nth-child(2) > :nth-child(2) > .sc-gweoQa > .sc-eIcdZJ > input').type(identi)
-  cy.get('.logradouro > .sc-gweoQa > .sc-eIcdZJ > input').type(rua)
-  cy.get('.numero > .sc-gweoQa > .sc-eIcdZJ > input').type(numero)
-  cy.get('.complemento > .sc-gweoQa > .sc-eIcdZJ > input').type(complemento)
-  cy.get('.OoyCV > :nth-child(1) > .sc-gweoQa > .sc-eIcdZJ > input').type(bairro)
-  cy.get('.OoyCV > :nth-child(2) > .sc-gweoQa > .sc-eIcdZJ > input').type(cidade)
-  cy.get('.OoyCV > :nth-child(3) > .sc-gweoQa > .sc-eIcdZJ > input').type(uf)
-  cy.get('.OoyCV > :nth-child(3) > .sc-gweoQa > .sc-eIcdZJ > input').click()
 
 
   return userInfo
